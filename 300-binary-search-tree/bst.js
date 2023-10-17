7c069caffde77240a78945c340c220bd13c92e87
Simsek Mert <simsek97@gmail.com>
2023-10-16 20:13:19 -0400

    Update bst with search and print methods

--- 300-binary-search-tree/bst.js
+++ 300-binary-search-tree/bst.js
@@ -1,58 +1,77 @@
 /**
- * @class Node
+ * @class BinarySearchTree
  */
-class Node {
+class BinarySearchTree {
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
-}
 
-/**
- * @class BinarySearchTree
- */
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
+  /**
+   * This method adds a new node to the binary search tree
+   *
+   * @param BinarySearchTree root
+   * @param number value
+   */
+  insertNode(root, value) {
+    const newNode = new BinarySearchTree(value);
 
-  insert(value) {
-    const newNode = new Node(value);
-
-    if (this.root === null) {
-      this.root = newNode;
+    if (newNode.value <= root.value) {
+      if (root.left) this.insertNode(root.left, value);
+      else root.left = newNode;
+    } else if (newNode.value > root.value) {
+      if (root.right) this.insertNode(root.right, value);
+      else root.right = newNode;
     } else {
-      this.insertNode(this.root, newNode);
+      return 'Invalid data';
     }
   }
 
-  insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-      if (node.left === null) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
+  /**
+   * This method searches the binary search tree for a given number x
+   *
+   * @param BinarySearchTree root
+   * @param number x
+   * @return
+   */
+  searchTree(root, x) {
+    if (!root) return console.log(`${x} is not found!`);
+    if (root.value === x) return console.log(`${x} is found!`);
+    else if (root.value <= x) this.searchTree(root.right, x);
+    else this.searchTree(root.left, x);
+  }
+
+  /**
+   * This method prints all element in a binary search tree
+   *
+   * @param BinarySearchTree root
+   */
+  printTree(root) {
+    console.log(root.value);
+    if (root.left) this.printTree(root.left);
+    if (root.right) this.printTree(root.right);
   }
 }
 
-// Example usage
-const bst = new BinarySearchTree();
+// Test use case
 
-bst.insert(10);
-bst.insert(5);
-bst.insert(15);
-bst.insert(3);
-bst.insert(7);
-bst.insert(12);
-bst.insert(18);
+// Create a new Binary Search Tree with a value of 9
+const bst = new BinarySearchTree(9);
 
-console.log(bst.root);
+// Add new nodes
+bst.insertNode(bst, 10);
+bst.insertNode(bst, 5);
+bst.insertNode(bst, 15);
+bst.insertNode(bst, 3);
+bst.insertNode(bst, 7);
+bst.insertNode(bst, 12);
+bst.insertNode(bst, 18);
+
+// Print the tree
+bst.printTree(bst);
+
+// Search for some values
+bst.searchTree(bst, 14);
+bst.searchTree(bst, 15);
+bst.searchTree(bst, 16);
